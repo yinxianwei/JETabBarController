25a8ca1a4a0e573c0828d190d2b7be2f18a530a3
尹现伟 <yinxianwei@qq.com>
2014-08-14 10:41:55 +0800

    JETabbarController

---
+++ JETabBarControllerTests/JETabBarControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  JETabBarControllerTests.m
+//  JETabBarControllerTests
+//
+//  Created by 尹现伟 on 14-8-14.
+//  Copyright (c) 2014年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JETabBarControllerTests : XCTestCase
+
+@end
+
+@implementation JETabBarControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
